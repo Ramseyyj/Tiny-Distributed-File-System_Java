6dff0c90166324f087d541f7816faa613e7849e2
Thomas <Thomas.benhamou@hotmail.fr>
2015-11-18 18:17:08 +0100

    // Create master and register it

--- src/main/java/fr/unice/miage/sd/tinydfs/main/MasterMain.java
+++ src/main/java/fr/unice/miage/sd/tinydfs/main/MasterMain.java
@@ -1,16 +1,30 @@
 package fr.unice.miage.sd.tinydfs.main;
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import fr.unice.miage.sd.tinydfs.nodes.Master;
+import fr.unice.miage.sd.tinydfs.nodes.MasterImpl;
+
+
 
 public class MasterMain {
 
 	// Usage: java fr.unice.miage.sd.tinydfs.main.MasterMain storage_service_name dfs_root_folder nb_slaves
-	public static void main(String[] args) {
+	public static void main(String[] args) throws RemoteException, AlreadyBoundException {
 		String storageServiceName = args[0];
 		String dfsRootFolder = args[1];
 		int nbSlaves = Integer.parseInt(args[2]);
 		
 		// Create master and register it
+		Master objMaster= new MasterImpl();
+		Registry registry = LocateRegistry.createRegistry(1099);
+		registry.bind("masterService", objMaster);
+		   System.out.println("Master prêt et disponible à l'adresse [hostname]/masterService");
 
+		
 	}
 	
 }
---
+++ src/main/java/fr/unice/miage/sd/tinydfs/nodes/MasterImpl.java
@@ -0,0 +1,49 @@
+package fr.unice.miage.sd.tinydfs.nodes;
+
+import java.io.File;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class MasterImpl extends UnicastRemoteObject implements Master {
+
+	public MasterImpl() throws RemoteException {
+		super();
+	}
+
+	@Override
+	public String getDfsRootFolder() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getNbSlaves() throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void saveFile(File file) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveBytes(String filename, byte[] fileContent) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public File retrieveFile(String filename) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] retrieveBytes(String filename) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
