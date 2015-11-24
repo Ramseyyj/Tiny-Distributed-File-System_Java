311986126fdab01f987c576100ccab50a323dad2
michel <michel@10.154.117.249>
2015-11-24 08:07:18 +0100

    vérification des paramètres de création du master et du slave et check si le slave ayant les numeros n'ont pas déja été enregistrés dans le rmi

--- src/main/java/fr/unice/miage/sd/tinydfs/main/MasterMain.java
+++ src/main/java/fr/unice/miage/sd/tinydfs/main/MasterMain.java
@@ -17,27 +17,34 @@ public class MasterMain {
 
 	// Usage: java fr.unice.miage.sd.tinydfs.main.MasterMain storage_service_name dfs_root_folder nb_slaves
 	public static void main(String[] args) throws RemoteException, AlreadyBoundException {
-		String storageServiceName = args[0];
-		String dfsRootFolder = args[1];
-		int nbSlaves = Integer.parseInt(args[2]);
 		
-		// Create master and register it
-		Registry registry = LocateRegistry.createRegistry(1099);
-		
-		Master objMaster;
-		try {
-			 objMaster = new MasterImpl(dfsRootFolder,nbSlaves);
-			 registry.bind(storageServiceName, objMaster);
-			 System.out.println("Master prêt et disponible à l'adresse: rmi://"+InetAddress.getLocalHost().getHostAddress()+
-					 ":1099/"+storageServiceName);
-		} catch (WrongNbSlaveException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-			System.out.println("Erreur: " + e.getMessage());
-		}catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Erreur: " + e.getMessage());
+		if(args.length==3)
+		{
+			String storageServiceName = args[0];
+			String dfsRootFolder = args[1];
+			int nbSlaves = Integer.parseInt(args[2]);
+			
+			// Create master and register it
+			Registry registry = LocateRegistry.createRegistry(1099);
+			
+			Master objMaster;
+			try {
+				 objMaster = new MasterImpl(dfsRootFolder,nbSlaves);
+				 registry.bind(storageServiceName, objMaster);
+				 System.out.println("Master prêt et disponible à l'adresse: rmi://"+InetAddress.getLocalHost().getHostAddress()+
+						 ":1099/"+storageServiceName);
+			} catch (WrongNbSlaveException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+				System.out.println("Erreur: " + e.getMessage());
+			}catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Erreur: " + e.getMessage());
+			}
+		}else{
+			System.out.println("ERROR NOMBRE DE PARAMETRE INCORRECT");
 		}
+		
 		
 		
 
--- src/main/java/fr/unice/miage/sd/tinydfs/main/SlaveMain.java
+++ src/main/java/fr/unice/miage/sd/tinydfs/main/SlaveMain.java
@@ -3,9 +3,12 @@ package fr.unice.miage.sd.tinydfs.main;
 import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RMISecurityManager;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+import fr.unice.miage.sd.tinydfs.exceptions.SlaveAlreadyRegisteredException;
 import fr.unice.miage.sd.tinydfs.nodes.Slave;
 import fr.unice.miage.sd.tinydfs.nodes.SlaveImpl;
 
@@ -13,21 +16,61 @@ public class SlaveMain {
 	
 	// Usage: java fr.unice.miage.sd.tinydfs.main.SlaveMain master_host dfs_root_folder slave_identifier
 	public static void main(String[] args) throws RemoteException, AlreadyBoundException {
-		String masterHost = args[0];
-		String dfsRootFolder = args[1];
-		int slaveId = Integer.parseInt(args[2]);
 		
-		// Create slave and register it (registration name must be "slave" + slave identifier)
-		Slave objSlave= new SlaveImpl(slaveId, dfsRootFolder);
-		try {
-			String url = "rmi://" + masterHost + "/slave" + slaveId;
-			Naming.rebind(url, objSlave);
-		} catch (MalformedURLException e) {
-			System.err.println("Error :\n");
-			e.printStackTrace();
+		if(args.length==3)
+		{
+			String masterHost = args[0];
+			String dfsRootFolder = args[1];
+			int slaveId = Integer.parseInt(args[2]);
+			
+			// Create slave and register it (registration name must be "slave" + slave identifier)
+			Slave objSlave= new SlaveImpl(slaveId, dfsRootFolder);
+			try {
+				String url = "rmi://" + masterHost + "/slave" + slaveId;
+				
+				boolean result = checkRegisterSlave(url, slaveId);
+				if(!result)
+				{
+					Naming.rebind(url, objSlave);
+					System.out.println("slave "+slaveId + " enregistré dans le RMI");
+				}
+				
+			}
+			catch (SlaveAlreadyRegisteredException e)
+			{
+				System.out.println("ERROR: " + e.getMessage());
+			}catch (MalformedURLException e) {
+				System.err.println("Error :\n");
+				e.printStackTrace();
+			} 
+		}else{
+			System.out.println("ERROR NOMBRE DE PARAMETRE INCORRECT");
 		}
-		System.out.println("slave"+slaveId + " enregistré dans le RMI");
+		
+		
 
+	}
+	
+	public static boolean checkRegisterSlave(String uri, int slaveId) throws SlaveAlreadyRegisteredException
+	{
+		boolean result =true;
+		try {
+			Remote r = Naming.lookup(uri);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			result=false;
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			result=false;
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			result=false;
+		}
+		if(result)
+		{
+			throw new SlaveAlreadyRegisteredException(slaveId);
+		}
+		return result;
 	}
 
 }
