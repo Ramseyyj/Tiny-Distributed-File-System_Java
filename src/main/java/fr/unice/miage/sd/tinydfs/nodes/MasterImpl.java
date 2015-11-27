0d65efb83c36bfbb8239eb41c4336d0144a3c717
Thomas <Thomas.benhamou@hotmail.fr>
2015-11-27 14:56:51 +0100

    echeeeeec :(

--- src/main/java/fr/unice/miage/sd/tinydfs/nodes/MasterImpl.java
+++ src/main/java/fr/unice/miage/sd/tinydfs/nodes/MasterImpl.java
@@ -241,12 +241,7 @@ public class MasterImpl extends UnicastRemoteObject implements Master {
 		}
 
 		public byte[] getComposedArray() {
-			try {
-				wait (2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 			return composedArray;
 		}
 
