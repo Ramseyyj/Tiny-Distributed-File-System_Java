96c51d5d2e454262caec4b361b303cb75e563a96
remslemnms <remi.kaeffer@gmail.com>
2015-12-06 18:53:00 +0100

    Gestion de retour null de la part des slaves dans le master

--- src/main/java/fr/unice/miage/sd/tinydfs/nodes/MasterImpl.java
+++ src/main/java/fr/unice/miage/sd/tinydfs/nodes/MasterImpl.java
@@ -104,6 +104,9 @@ public class MasterImpl extends UnicastRemoteObject implements Master {
 	@Override
 	public File retrieveFile(String filename) throws RemoteException {
 		byte[] b = retrieveBytes(filename);
+		if(b == null) {
+			return null;
+		}
 		File res = new File(dfsRootFolder + File.separator + filename);
 		try {
 			if(!res.exists()) {
@@ -135,6 +138,9 @@ public class MasterImpl extends UnicastRemoteObject implements Master {
 			}
 		}
 		List<byte[]> bLeft = this.leftSlave.subRetrieve(filename);
+		if(bLeft == null) {
+			return null;
+		}
 		List<byte[]> bRight = this.rightSlave.subRetrieve(filename);
 		return getRecomposeByteArray(bLeft, bRight);
 	}
